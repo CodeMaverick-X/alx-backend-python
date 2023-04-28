306002ea453fcda7d08518d3de6a533d7f1baad6
reinhard <reinhard2057@gmail.com>
2023-04-28 18:00:59 +0000

    task advanced

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Augmented the following code
+with the correct duck-typed annotations"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Safe first element: types of the element not known
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module: element of a sequence anotation
+"""
+from typing import Mapping, Any, Union, TypeVar
+Tv = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[Tv, None] = None) -> Union[Any, T]:
+    """
+    return values safely
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
