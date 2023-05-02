bd30cb43d9127e0b04c6491b9c6f44edb2907114
reinhard <reinhard2057@gmail.com>
2023-05-02 21:51:11 +0000

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 """
 an asynchronous coroutine that takes in an int
-arg max_delay with default value 10, named
-wait_random that waits for a random delay betwen 0
-and max_delay(included float value) seconds and
-eventually returns it.
-uses `random` module
+and returns teh val and waits.
 """
 
 import asyncio
@@ -15,10 +11,9 @@ from typing import Union
 
 async def wait_random(max_delay: int = 10) -> Union[int, float]:
     """wait with async function
-    return the random number after
-    waiting
+    return the random num aftr waiting
     """
     r = random.uniform(0, max_delay)
-    await asyncio.sleep(r)
+    await asyncio.sleep(1)
 
     return r
