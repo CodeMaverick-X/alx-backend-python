10c446696fe6950c9457448ffe379fd4a5a4c13a
reinhard <reinhard2057@gmail.com>
2023-05-03 00:00:36 +0000

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+contains a routine wait_ n that
+takes in 2 in arg n and max_delay
+and spawns wait_random n times
+"""
+
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """returns list of floats"""
+    res = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+
+    return res
