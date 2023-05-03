4dd1a9c4adf134f0f6db8acc562d3bd706f46d83
reinhard <reinhard2057@gmail.com>
2023-05-03 00:06:26 +0000

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -13,4 +13,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """returns list of floats"""
     res = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
 
-    return res
+    return sorted(res)
