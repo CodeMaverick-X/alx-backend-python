2b3866b07ba02013c397c26138ffda4f62599fc6
reinhard <reinhard2057@gmail.com>
2023-05-03 11:39:46 +0000

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ from typing import Generator
 import asyncio
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """yields an int"""
     for _ in range(10):
         await asyncio.sleep(1)
