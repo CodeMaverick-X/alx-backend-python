1fa9da3d68db87aafc4765366372d8ed5e4ae9e1
CodeMaverick-X <reinhard2057@gmail.com>
2025-06-04 23:00:16 +0100

    some db shit

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,13 @@
+import time
+import sqlite3
+import functools
+
+class DatabaseConnection:
+    def __enter__(self):
+        print("Opening database connection")
+        self._conn =  sqlite3.connect('users.db')
+        return "Database Connection"
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("Closing database connection")
+        self._conn.close()
+        return True
