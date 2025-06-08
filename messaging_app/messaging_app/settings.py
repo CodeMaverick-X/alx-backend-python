77a28cbc73de8b2f51b25cdf6a5d344b56508d50
CodeMaverick-X <reinhard2057@gmail.com>
2025-06-08 12:52:07 +0100

    some extra shit

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -72,9 +72,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',  # Optional but common
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ]
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
 
 # Database
