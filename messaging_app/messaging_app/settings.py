eeea58c70698f83d0d569512f132af3b8a0d36d3
CodeMaverick-X <reinhard2057@gmail.com>
2025-06-08 12:50:13 +0100

    some extra shit

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -71,6 +71,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
