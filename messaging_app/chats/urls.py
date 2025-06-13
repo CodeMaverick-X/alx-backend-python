876a39ad1dd7e32ec6f3985a7cdb54631811b765
CodeMaverick-X <reinhard2057@gmail.com>
2025-06-13 16:35:32 +0100

    correct and fix

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
 urlpatterns = [
     
 ]
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('conversations', views.ConversationViewSet)
 router.register('messages', views.MessageViewSet)
 
